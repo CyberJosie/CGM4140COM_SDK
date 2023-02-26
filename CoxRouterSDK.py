fa4fb46300719d25430848098173cdba63787168
user <user@debian.user>
2023-02-26 13:27:19 -0700

    Add more features

--- CoxRouterSDK.py
+++ CoxRouterSDK.py
@@ -1,8 +1,11 @@
-import requests
 import json
+import uuid
+import urllib
+import urllib3
+import requests
 from requests import session
 from bs4 import BeautifulSoup
-
+from colorama import Fore
 
 def cleanval(val: str) -> str:
     repl = {
@@ -16,9 +19,55 @@ def cleanval(val: str) -> str:
     return val
 
 
-class CoxGatewaySDK:
-    def __init__(self, host: str = '192.168.0.1'):
+class Device():
+    def __init__(self, name: str, connected: bool, network: str, lease_type: str, ipv4: str, ipv6: str, mac: str):
+        self.name: str = name
+        self.connected: bool = connected
+        self.network: str = network
+        self.lease_type: str = lease_type
+        self.ipv4: str = ipv4
+        self.ipv6: str = ipv6
+        self.mac: str = mac
+
+    def pretty(self) -> str:
+        device_pretty = """
+[Device: {c3}{_1}{r} | Connected: {cc}{_2}{r}]
+ {c1}Network:{c2} {_3}{r}
+ {c1}Lease Type:{c2} {_4}{r}
+ {c1}IPv4:{c2} {_5}{r}
+ {c1}IPv6:{c2} {_6}{r}
+ {c1}MAC:{c2} {_7}{r}""".format(
+            c1=Fore.CYAN,
+            c2=Fore.LIGHTYELLOW_EX,
+            c3=Fore.LIGHTBLUE_EX,
+            r=Fore.RESET,
+            cc=Fore.GREEN if self.connected else Fore.RED,
+            
+
+            _1=self.name,
+            _2='Yes' if self.connected else 'No',
+            _3=self.network,
+            _4=self.lease_type.upper() if self.lease_type == 'dhcp' else self.lease_type.title(),
+            _5=self.ipv4,
+            _6=self.ipv6,
+            _7=self.mac,)
+        return device_pretty
+
+
+class Gateway:
+    """
+    """
+
+    def __init__(self, host: str = '192.168.0.1', verbose: bool = False, use_https: bool = False):
         self.host = host
+        self.verbose = verbose
+        self.use_https = use_https
+
+        if use_https:
+            self.full_host = 'https://{}'.format(self.host)
+        else:
+            self.full_host = 'http://{}'.format(self.host)
+
         self.session_cookie = None
         self.sess = requests.session()
 
@@ -27,7 +76,7 @@ class CoxGatewaySDK:
             'Cache-Control': 'max-age=0',
             'Upgrade-Insecure-Requests': '1',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Referer': 'http://{}'.format(self.host),
+            'Referer': self.full_host,
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'close',
@@ -38,8 +87,15 @@ class CoxGatewaySDK:
         return True if self.session_cookie != None else False
 
     def authenticate(self, username: str, password: str) -> bool:
+        """
+        Authenticated with the gatewat using given credentials
+
+        :param username: Username to authenticate with
+        :param password: Password to authenticate with
+        """
         logged_in = False
         session_cookie = None
+        data = ''
 
         url_enc_data = 'username={}&password={}&locale=false'.format(
             username, password)
@@ -49,11 +105,10 @@ class CoxGatewaySDK:
             'Content-Length': str(len(url_enc_data)),
             'Cache-Control': 'max-age=0',
             'Upgrade-Insecure-Requests': '1',
-            'Origin': 'http://{}'.format(self.host),
+            'Origin': self.full_host,
             'Content-Type': 'application/x-www-form-urlencoded',
-            # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.78 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Referer': 'http://{}'.format(self.host),
+            'Referer': self.full_host,
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'close',
@@ -65,32 +120,41 @@ class CoxGatewaySDK:
             'locale': False,
         }
 
-        url = 'http://{}/check.jst'.format(self.host)
+        if self.verbose:
+            print(' Authenticating with {} as {}'.format(self.host, username))
+
+        url = '{}/check.jst'.format(self.full_host)
         try:
             r = self.sess.post(url, headers=headers, data=params)
-
+            data = r.content.decode()
             if 'Set-Cookie' in list(r.headers.keys()):
                 session_cookie = r.headers.get('Set-Cookie')
                 logged_in = True
         except Exception as e:
             print("Error logging in: {}".format(str(e)))
         self.session_cookie = session_cookie
+
+        if self.verbose:
+            if logged_in:
+                print(' Logged in')
+            else:
+                print(' Authentication failed')
+                print(' Server Response:\n{}'.format(data))
         return logged_in
 
-    def devices(self) -> dict:
-        devices = {
-            'online': [],
-            'offline': [],
-        }
-        url = 'http://{}/connected_devices_computers.jst'.format(self.host)
+    def devices(self) -> list[Device]:
+        """
+        Returns a list of devices known by the gateway as python objects
+        """
+        odevices = []
+        url = '{}/connected_devices_computers.jst'.format(self.full_host)
 
         if not self.has_session():
-            print("You need to authenticate first!")
+            print(" You are unauthorized, start by authenticating.")
             return []
 
-        url = 'http://{}/connected_devices_computers.jst'.format(self.host)
         try:
-            r = self.sess.post(url, headers=self.GET_headers)
+            r = self.sess.get(url, headers=self.GET_headers)
 
             soup = BeautifulSoup(str(r.content.decode()), 'html.parser')
 
@@ -126,7 +190,6 @@ class CoxGatewaySDK:
                     # advanced info
                     adv_info = hostname_block.find('div')
                     for child in adv_info.find_all('dd'):
-
                         if 'ipvaddloc' in str(child):
                             ipv4_address = str(child.next_sibling)
                         if 'ipv6addloc' in str(child):
@@ -134,16 +197,17 @@ class CoxGatewaySDK:
                         if 'macaddlocnew' in str(child):
                             mac_address = str(child.next_sibling)
 
-                    devices['online'].append({
-                        'name': name,
-                        'lease_type': res_type,
-                        'network': network,
-                        'rssi': rssi_level,
-                        'ipv4': ipv4_address,
-                        'ipv6': ipv6_address,
-                        'mac': mac_address,
-                    })
+                    odevices.append(Device(
+                        name=name,
+                        connected=True,
+                        network=network,
+                        lease_type=res_type,
+                        ipv4=ipv4_address,
+                        ipv6=ipv6_address,
+                        mac=mac_address))
                 except Exception as e:
+                    if self.verbose:
+                        print(' A non-fatal exception occured while parsing a device: {}'.format(str(e)))
                     continue
 
             for odr in offline_device_rows:
@@ -174,29 +238,38 @@ class CoxGatewaySDK:
                         if 'macaddlocnew' in str(child):
                             mac_address = str(child.next_sibling)
 
-                    devices['offline'].append({
-                        'name': name,
-                        'lease_type': res_type,
-                        'network': network,
-                        'ipv4': ipv4_address,
-                        'ipv6': ipv6_address,
-                        'mac': mac_address,
-                    })
+                    odevices.append(Device(
+                        name=name,
+                        connected=False,
+                        network=network,
+                        lease_type=res_type,
+                        ipv4=ipv4_address,
+                        ipv6=ipv6_address,
+                        mac=mac_address))
                 except Exception as e:
+                    if self.verbose:
+                        print(' A non-fatal exception occured while parsing a device: {}'.format(str(e)))
                     continue
-
         except Exception as e:
-            print('Error gathering devices: {}'.format(str(e)))
-            return {}
-        return devices
+            print(' Error gathering devices: {}'.format(str(e)))
+        return odevices
 
-    def online_devices(self) -> list:
-        return self.devices()['online']
+    def online_devices(self) -> list[Device]:
+        """
+        Returns known and online devices as python objects
+        """
+        return [d for d in self.devices() if d.connected]
 
-    def offline_devices(self) -> list:
-        return self.devices()['offline']
+    def offline_devices(self) -> list[Device]:
+        """
+        Returns known and online devices as python objects
+        """
+        return [d for d in self.devices() if not d.connected]
 
     def connection_status(self) -> dict:
+        """
+        Returns connection status information
+        """
         status = {
             'lan': {},
             'wan': {},
@@ -205,11 +278,11 @@ class CoxGatewaySDK:
         }
 
         if not self.has_session():
-            print("You need to authenticate first!")
-            return {}
+            print(" You are unauthorized, start by authenticating.")
+            return status
 
         try:
-            url = 'http://{}/connection_status.jst'.format(self.host)
+            url = '{}/connection_status.jst'.format(self.full_host)
             r = self.sess.get(url, headers=self.GET_headers)
 
             soup = BeautifulSoup(str(r.content.decode()), 'html.parser')
@@ -304,12 +377,13 @@ class CoxGatewaySDK:
 
             }
         }
+
         if not self.has_session():
-            print("You need to authenticate first!")
-            return {}
+            print(" You are unauthorized, start by authenticating.")
+            return creds
 
         try:
-            url = 'http://{}/at_a_glance.jst'.format(self.host)
+            url = '{}/at_a_glance.jst'.format(self.full_host)
             r = self.sess.get(url, headers=self.GET_headers)
 
             soup = BeautifulSoup(str(r.content.decode()), 'html.parser')
@@ -345,7 +419,11 @@ class CoxGatewaySDK:
             'note': 'the web developer left notes calling their code a mess, dont blame me here.'
         }
 
-        url = 'http://{}/software.jst'.format(self.host)
+        if not self.has_session():
+            print(" You are unauthorized, start by authenticating.")
+            return {}
+
+        url = '{}/software.jst'.format(self.full_host)
         try:
             r = self.sess.get(url, headers=self.GET_headers)
             soup = BeautifulSoup(str(r.content.decode()), 'html.parser')
@@ -363,3 +441,218 @@ class CoxGatewaySDK:
         except Exception as e:
             print('Error while parsing version info: {}'.format(str(e)))
         return version_info
+
+    def network_setup(self) -> dict:
+        setup_conf = {}
+
+        if not self.has_session():
+            print(" You are unauthorized, start by authenticating.")
+            return setup_conf
+
+        url = '{}/network_setup.jst'.format(self.full_host)
+        try:
+            r = self.sess.get(url, headers=self.GET_headers)
+            soup = BeautifulSoup(str(r.content.decode()), 'html.parser')
+
+            for row in soup.find_all('div', class_='form-row'):
+                value = cleanval(row.find('span', class_='value').text)
+
+                if 'Internet:' in str(row):
+                    setup_conf['internet_active'] = value
+
+                elif 'Local time:' in str(row):
+                    setup_conf['local_time'] = value
+
+                elif 'System Uptime:' in str(row):
+                    setup_conf['uptime'] = value
+
+                elif 'WAN IP Address (IPv4):' in str(row):
+                    setup_conf['wan_ipv4'] = value
+
+                elif 'WAN Default Gateway Address (IPv4):' in str(row):
+                    setup_conf['wan_default_ipv4'] = value
+
+                elif 'WAN IP Address (IPv6):' in str(row):
+                    setup_conf['wan_ipv6'] = value
+
+                elif 'WAN Default Gateway Address (IPv6):' in str(row):
+                    setup_conf['wan_default_ipv6'] = value
+
+                elif 'Delegated prefix (IPv6):' in str(row):
+                    setup_conf['delegated_ipv6_prefix'] = value
+
+                elif 'Primary DNS Server (IPv4):' in str(row):
+                    setup_conf['primary_ipv4_dns_server'] = value
+
+                elif 'Secondary DNS Server (IPv4):' in str(row):
+                    setup_conf['secondary_ipv4_dns_server'] = value
+
+                elif 'Primary DNS Server (IPv6):' in str(row):
+                    setup_conf['primary_ipv6_dns_server'] = value
+
+                elif 'Secondary DNS Server (IPv6):' in str(row):
+                    setup_conf['secondary_ipv6_dns_server'] = value
+
+        except Exception as e:
+            print('Error parsing network setup: {}'.format(str(e)))
+        return setup_conf
+
+    def filter_host(self, mac: str, name: str = str(uuid.uuid4()), action: int = 0, ssid: int = 1) -> bool:
+        """
+        Allow/Block connections from a device by it's MAC address.
+
+        :param mac: MAC address of the device
+        :param action: Action to take when a device with this MAC
+                       address connects to the router.
+                       Options:
+                        0 - deny
+                        1 - allow
+        :param ssid: Wireless network this rule applies to.
+                     Options:
+                     1 - 2.4GHz Network
+                     2 - 5GHz Network
+        """
+        success = False
+        filter_mode = 'deny' if action == 0 else 'allow'
+
+        if not self.has_session():
+            print(" You are unauthorized, start by authenticating.")
+            return False
+
+        data = {
+            "configInfo": "{\"ssid_number\":\"" + str(ssid) + "\",\"filtering_mode\":\"" + filter_mode + "\",\"ft\":[[\"" + name + "\",\"" + mac + "\"]],\"target\":\"save_filter\"}"
+        }
+
+        headers = {
+            'Host': self.host,
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'Origin': self.full_host,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Accept': '*/*',
+            'Referer': '{}/wireless_network_configuration.jst'.format(self.full_host),
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'close',
+            'Cookie': self.session_cookie,
+        }
+        url = '{}/actionHandler/ajaxSet_wireless_network_configuration.jst'.format(
+            self.full_host)
+        try:
+            r = self.sess.post(url, headers=headers, data=data)
+            # print(r.content.decode())
+            if int(r.status_code) == 200:
+                success = True
+        except Exception as e:
+            print('Error filtering host: {}'.format(str(e)))
+        return success
+
+    def block_device(self, mac: str, network: int) -> None:
+        """
+        Blocks access to the network for a single device by its
+        MAC address.
+
+        :param mac: MAC address of the device.
+        :param ssid: Wireless network this rule applies to.
+                     Options:
+                     1 - 2.4GHz Network
+                     2 - 5GHz Network
+        """
+        self.filter_host(mac, action=0, ssid=network)
+
+    def unblock_device(self, mac: str, network: int) -> None:
+        """
+        Unblocks access to the network for a single device by its
+        MAC address.
+
+        :param mac: MAC address of the device.
+        :param ssid: Wireless network this rule applies to.
+                     Options:
+                     1 - 2.4GHz Network
+                     2 - 5GHz Network
+        """
+        self.filter_host(mac, action=1, ssid=network)
+
+    def query_device(self, ipv4=None, ipv6=None, mac=None, name=None, part_name=None) -> Device:
+        """
+        Get a python object representation of a device by its name, ipv4 address, 
+        ipv6 address, or mac address. Only use one at a time, not all values are always
+        filled. 
+
+        :param ipv4: Search a device by its ipv4
+        :param ipv6: Search a device by its ipv6
+        :param mac: Search a device by its mac
+        :param name: Search a device by its name
+        :param part_name: Search a device by a substring in its name.
+                          will return the first device with a name containing
+                          this substring. (not case sensitive.)
+        """
+        dev = Device()
+
+        if not self.has_session():
+            print(" You are unauthorized, start by authenticating.")
+            return dev
+
+        q_key = ''
+        value = None
+        if ipv4:
+            q_key = 'ipv4'
+            value = ipv4
+        elif ipv6:
+            q_key = 'ipv6'
+            value = ipv6
+        elif name:
+            q_key = 'name'
+            value = name
+        elif mac:
+            q_key = 'mac'
+            value = mac
+
+        device_list = []
+        devices = self.devices()
+
+        for d in devices['online']:
+            d['connected'] = True
+            device_list.append(d)
+
+        for d in devices['offline']:
+            d['connected'] = False
+            device_list.append(d)
+
+        for d in device_list:
+            # Either filter by partial name
+            if part_name is not None:
+                # Check if the lowercased device name contains
+                # the lwoercased filter substring
+                if part_name.lower() in d['name'].lower():
+                    dev.name = d['name']
+                    dev.network = d['network']
+                    dev.lease_type = d['lease_type']
+                    dev.ipv4 = d['ipv4']
+                    dev.ipv6 = d['ipv6']
+                    dev.mac = d['mac']
+                    dev.connected = d['connected']
+                    break
+
+            # Or exactly one of the keys
+            elif d[q_key] == value:
+                dev.name = d['name']
+                dev.network = d['network']
+                dev.lease_type = d['lease_type']
+                dev.ipv4 = d['ipv4']
+                dev.ipv6 = d['ipv6']
+                dev.mac = d['mac']
+                dev.connected = d['connected']
+                break
+
+        return dev
+
+    def logout(self) -> bool:
+        if self.verbose:
+            print(' Logging Out')
+        logged_out = False
+        r = self.sess.get(
+            '{}/home_loggedout.jst'.format(self.full_host), headers=self.GET_headers)
+        logged_out = True if int(r.status_code) == 302 else False
+        return logged_out
---
+++ clitool.py
@@ -0,0 +1,283 @@
+#!/usr/bin/python3
+import os
+import sys
+import json
+import time
+import argparse
+import textwrap
+from colorama import Fore
+from CoxRouterSDK import Gateway, Device
+
+"""
+These functions take the JSON results
+provided by the SDK and turn them into a legible,
+colored and formatted text output
+"""
+
+
+def get_all_devices(gw: Gateway) -> str:
+    result = ''
+    devices = gw.devices()
+    for d in devices:
+        result += d.pretty() + '\n'
+    result += ' There are {} total devices'.format(len(devices))
+    return result
+
+
+def get_online_devices(gw: Gateway) -> str:
+    result = ''
+    od = gw.online_devices()
+    for d in od:
+        result += d.pretty() + '\n'
+    result += ' There are {} connected devices'.format(len(od))
+    return result
+
+
+def get_offline_devices(gw: Gateway) -> str:
+    result = ''
+    od = gw.offline_devices()()
+    for d in od:
+        result += d.pretty() + '\n'
+    result += ' There are {} disconnected devices'.format(len(od))
+    return result
+
+
+def get_network_setup_info(gw: Gateway) -> str:
+    result = ''
+    data = gw.network_setup()
+    for d in list(data.keys()):
+        result += ' > {}\n    {}\n'.format(
+            '{}{}{}'.format(
+                Fore.CYAN,
+                d.replace('_', ' ').title(),
+                Fore.RESET,
+            ),
+            '{}{}{}'.format(
+                Fore.LIGHTYELLOW_EX,
+                data[d],
+                Fore.RESET,
+            ))
+    return result
+
+
+def get_connection_status(gw: Gateway) -> str:
+    result = ''
+    data = gw.connection_status()
+
+    for k in list(data.keys()):
+        # Network Name Tag
+        result += '{}[{}]{}\n'.format(
+            Fore.LIGHTCYAN_EX,
+            k,
+            Fore.RESET,
+        )
+        # Information for this network
+        for sk in list(data[k].keys()):
+            result += ' > {}\n    {}\n'.format(
+                '{}{}{}'.format(
+                    Fore.CYAN,
+                    sk.replace('_', ' ').title(),
+                    Fore.RESET,
+                ),
+                '{}{}{}'.format(
+                    Fore.LIGHTYELLOW_EX,
+                    data[k][sk],
+                    Fore.RESET,
+                ))
+    return result
+
+
+def main(args) -> None:
+    host = '192.168.0.1'
+    https_enabled = False
+    verbose = False
+    username = ''
+    password = ''
+
+    # Get credentials
+    if args.auth_file is not None:
+        try:
+            with open(args.auth_file, 'r') as f:
+                lines = f.read().split('\n')
+                username = lines[0].strip()
+                password = lines[1].strip()
+        except Exception as e:
+            print(' Error reading credentials from auth file!\n{}'.format(str(e)))
+            exit(1)
+    elif args.username is not None and args.password is not None:
+        username = args.username
+        password = args.password
+    else:
+        print('Credentials are required!')
+        exit(1)
+
+    # Get run options
+    if args.verbose is not None and args.verbose is not False:
+        verbose = True
+
+    if args.use_https is not None and args.use_https is not False:
+        https_enabled = True
+
+    if args.host is not None:
+        host = args.host
+
+    # Setup Gateway client
+    gw = Gateway(
+        host=host,
+        verbose=verbose,
+        use_https=https_enabled,
+    )
+
+    # Get actions
+    start = time.time()
+    something_happened = False
+
+    # List all devices
+    if args.list_devices is not None and args.list_devices is not False:
+        print(' Getting all devices...')
+        gw.authenticate(username, password)
+        result = get_all_devices(gw)
+        print(result)
+        something_happened = True
+
+    # List online devices
+    elif args.connected is not None and args.connected is not False:
+        print(' Getting connected devices...')
+        gw.authenticate(username, password)
+        result = get_online_devices(gw)
+        print(result)
+        something_happened = True
+
+    # List offline devices
+    elif args.disconnected is not None and args.disconnected is not False:
+        print(' Getting disconnected devices...')
+        gw.authenticate(username, password)
+        result = get_offline_devices(gw)
+        print(result)
+        something_happened = True
+
+    elif args.net_setup is not None and args.net_setup is not False:
+        print(' Getting network setup information...')
+        gw.authenticate(username, password)
+        result = get_network_setup_info(gw)
+        print(result)
+        something_happened = True
+
+    elif args.conn_status is not None and args.conn_status is not False:
+        print(' Getting network connection status...')
+        gw.authenticate(username, password)
+        result = get_connection_status(gw)
+        print(result)
+        something_happened = True
+
+    if something_happened:
+        # Log out
+        gw.logout()
+
+        finish = time.time()
+        print('Elapsed: {}s'.format(round(finish-start, 4)))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog='CGM4140COM CLI Tool',
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=textwrap.dedent('''
+        (3rd party) CLI Tool for CGM4140COM gateways.
+        '''),
+    )
+
+    parser.add_argument(
+        '--host', '-H',
+        action='store',
+        default='192.168.0.1',
+        help=textwrap.dedent('''
+        IP address of the gateway, default is 192.168.0.1.
+        ''')
+    )
+
+    parser.add_argument(
+        '--username', '-u',
+        action='store',
+        help=textwrap.dedent('''
+        Username to authenticate with. Required in each command
+        ''')
+    )
+
+    parser.add_argument(
+        '--password', '-p',
+        action='store',
+        help=textwrap.dedent('''
+        Password to authenticate with. Required in each command
+        ''')
+    )
+
+    parser.add_argument(
+        '--auth-file', '-a',
+        action='store',
+        help=textwrap.dedent('''
+        Path to text file containing authentication credentials.
+        First line stores username, second line store password. 
+        ''')
+    )
+
+    parser.add_argument(
+        '--list-devices', '-ld',
+        action='store_true',
+        help=textwrap.dedent('''
+        Show all online and offline devices known by this gateway.
+        ''')
+    )
+
+    parser.add_argument(
+        '--connected', '-cd',
+        action='store_true',
+        help=textwrap.dedent('''
+        Show all online devices known by this gateway.
+        ''')
+    )
+    parser.add_argument(
+        '--disconnected', '-dd',
+        action='store_true',
+        help=textwrap.dedent('''
+        Show all online devices known by this gateway.
+        ''')
+    )
+
+    parser.add_argument(
+        '--conn-status', '-cs',
+        action='store_true',
+        help=textwrap.dedent('''
+        Show Connection Status (General connection info for main wired and
+        wireless networks)
+        ''')
+    )
+
+    parser.add_argument(
+        '--net-setup', '-ns',
+        action='store_true',
+        help=textwrap.dedent('''
+        Show Network Setup Information (Advanced connection information)
+        ''')
+    )
+
+    parser.add_argument(
+        '--use-https', '-S',
+        action='store_true',
+        help=textwrap.dedent('''
+        Use the routers HTTPS web service (if available, not enabled by default)
+        ''')
+    )
+
+    parser.add_argument(
+        '--verbose', '-v',
+        action='store_true',
+        help=textwrap.dedent('''
+        Run actions in verbose mode. Helpful for debugging
+        and looking cool.
+        ''')
+    )
+
+    args = parser.parse_args()
+    main(args)
